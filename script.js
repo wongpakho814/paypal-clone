95009ded4f1702430ae9efa766a6d68a8e3004e4
wongpakho814 <wongpakho814@gmail.com>
2023-05-16 21:15:22 +1000

    Implemented JS functionality to switch between pages on button click

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Pages
+document.querySelectorAll(".logo").forEach((logo) => {
+  logo.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "block";
+    document.querySelector(".login-page").style.display = "none";
+    document.querySelector(".signup-page").style.display = "none";
+  });
+});
+
+document.querySelectorAll(".login").forEach((loginBtn) => {
+  loginBtn.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "none";
+    document.querySelector(".login-page").style.display = "block";
+    document.querySelector(".signup-page").style.display = "none";
+  });
+});
+
+document.querySelectorAll(".signup").forEach((signupBtn) => {
+  signupBtn.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "none";
+    document.querySelector(".login-page").style.display = "none";
+    document.querySelector(".signup-page").style.display = "flex";
+  });
+});
+// End of pages
+
 // Nav
 const dropdownItems = document.querySelectorAll(".dropdown-hover");
 
